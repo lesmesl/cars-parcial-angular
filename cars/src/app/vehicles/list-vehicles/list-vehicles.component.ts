c5c5fb5c78578c24ec745efa7b31e89eafd2bd22
camilo lesmes <c.lesmesl@uniandes.edu.co>
2023-11-05 22:15:09 -0500

    total funcional

--- cars/src/app/vehicles/list-vehicles/list-vehicles.component.ts
+++ cars/src/app/vehicles/list-vehicles/list-vehicles.component.ts
@@ -9,12 +9,27 @@ import { Vehicle } from '../vehicles'
 })
 export class ListVehiclesComponent {
   vehicles: Vehicle[] = [];
+  brands: { [marca: string]: number } = {};
+  brandsList: {nombre:string, cantidad:number}[] = [];
 
   constructor(private vehicleService: VehicleService) {}
 
   ngOnInit() {
     this.vehicleService.getVehiclesList().subscribe((vehicles) => {
       this.vehicles = vehicles
+
+      this.vehicles.forEach(coche => {
+        const marca = coche.marca;
+        this.brands[marca] = (this.brands[marca] || 0) + 1;
+      });
+
+      for (const marca in this.brands) {
+        if (this.brands.hasOwnProperty(marca)) {
+            const cantidad = this.brands[marca];
+            this.brandsList.push({nombre: marca, cantidad})
+        }
+      }
+     
     })
   }
 }
