5984936e0baf5307fbd8063cd5cea0147e843006
camilo lesmes <c.lesmesl@uniandes.edu.co>
2023-11-05 23:50:13 -0500

    Test ok

--- cars/src/app/vehicles/list-vehicles/list-vehicles.component.spec.ts
+++ cars/src/app/vehicles/list-vehicles/list-vehicles.component.spec.ts
@@ -1,21 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListVehiclesComponent } from './list-vehicles.component';
+import { VehicleService } from '../vehicles.service';
+import { Vehicle } from '../vehicles';
 
 describe('ListVehiclesComponent', () => {
   let component: ListVehiclesComponent;
   let fixture: ComponentFixture<ListVehiclesComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListVehiclesComponent ],
+      providers: [ VehicleService ]
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ListVehiclesComponent]
-    });
     fixture = TestBed.createComponent(ListVehiclesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the tabladata table correctly', () => {
+    // Crear una lista de 3 vehículos
+    const vehicles: Vehicle[] = [
+      { id: 1, marca: 'Renault', linea: 'Kangoo', modelo: 2017 },
+      { id: 2, marca: 'Chevrolet', linea: 'Spark', modelo: 2018 },
+      { id: 3, marca: 'Chevrolet', linea: 'Sail', modelo: 2016 }
+    ];
+
+    // Establecer la lista de vehículos en el componente
+    component.vehicles = vehicles;
+
+    // Detectar los cambios
+    fixture.detectChanges();
+
+    // Obtener la tabla `tabladata`
+    const table: HTMLTableElement = fixture.nativeElement.querySelector('#tabladata');
+
+    // Verificar que la tabla tenga 4 filas (1 fila para el encabezado y 3 filas para los datos)
+    const rowCount = table.rows.length;
+    expect(rowCount).toBe(4);
   });
 });
--- cars/src/app/vehicles/vehicles.ts
+++ cars/src/app/vehicles/vehicles.ts
@@ -2,9 +2,5 @@ export interface Vehicle {
     "id": number,
     "marca": string,
     "linea": string,
-    "referencia": string,
     "modelo": number,
-    "kilometraje": number,
-    "color": string,
-    "imagen": string
 }
